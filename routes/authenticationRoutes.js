2ebd013efd711cb485a324e1b48549c18e3136b5
rafmasloman <raflymasloman20@gmail.com>
2022-06-04 23:55:18 +0800

    mengubah method /login menjadi POST

--- routes/authenticationRoutes.js
+++ routes/authenticationRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const authenticationController = require('../controller/authenticationController');
 
 const router = express.Router();
-router.get('/login', authenticationController.login);
+router.post('/login', authenticationController.login);
 router.post(
   '/register',
   authenticationController.registerValidation,
